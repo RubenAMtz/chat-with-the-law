4c1f9fbd6c785a7575de2aa0cf05606f8833118d
Ruben <ruben.amtz@outlook.com>
2023-05-25 20:16:11 +0000

    added script to download embeddings

--- get_embeddings.py
+++ get_embeddings.py
@@ -1,22 +1,32 @@
 # download blob from azure storage
 
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient
 import os
 from dotenv import load_dotenv
-import json
-import numpy as np
 
 dotenv_path = f".env.prod"
 load_dotenv(dotenv_path=dotenv_path)
 
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
 container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
 
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+print(f"Connection string: {connection_string}")
+print(f"Key: {key}")
+print(f"Container name: {container_name}")
+
+blob_service_client = BlobServiceClient.from_connection_string(connection_string, credential=key)
 container_client = blob_service_client.get_container_client(container_name)
 
-# download blob from azure storage
-blob_client = blob_service_client.get_blob_client(container=container_name, blob="embeddings/leyes_chromadb_512_40_ada_002_recursive/index/index_7f34d7a0-1444-407e-ac98-63e657161537.bin")
-downloaded_blob = blob_client.download_blob()
-with open("index_7f34d7a0-1444-407e-ac98-63e657161537.bin", "wb") as my_blob:
-    my_blob.write(downloaded_blob.readall())
+# download blobs recursively from from azure storage folder
+embeddings_folder = 'leyes_chromadb_512_40_ada_002_recursive'
+
+blobs = container_client.list_blobs(name_starts_with=embeddings_folder)
+for blob in blobs:
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob.name)
+    downloaded_blob = blob_client.download_blob()
+    if not os.path.exists(blob.name):
+        os.makedirs(os.path.dirname(blob.name), exist_ok=True)
+    with open(blob.name, "wb") as my_blob:
+        my_blob.write(downloaded_blob.readall())
+
